0d8b512de404b63d94d8e6da202335c0a79d3342
Mathavik <mathavikanagaraj1@gmail.com>
2026-06-05 12:47:18 +0530

    smart fronend

--- smart-ledger/src/store/useStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-export const useStore = create((set) => ({
-  cart: [],
-  addToCart: (item) =>
-    set((state) => ({ cart: [...state.cart, item] })),
-}));
--- smart-ledger/src/utils/helpers.js
+++
@@ -1,3 +0,0 @@
-export const calculateTotal = (cart) => {
-  return cart.reduce((acc, item) => acc + item.price * item.qty, 0);
-};
