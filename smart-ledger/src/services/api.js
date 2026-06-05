9a13c9cf7da6b85b4d719537d8fff67f86bc9b4a
Mathavik <mathavikanagaraj1@gmail.com>
2026-06-05 12:44:33 +0530

    smart fronend

---
+++ smart-ledger/src/services/api.js
@@ -0,0 +1,25 @@
+﻿import axios from "axios";
+
+export const API_BASE_URL = "http://127.0.0.1/ecommerce-billing/smart-ledger-backend/api/";
+
+// Axios instance
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+// Keep JSON content-type for normal object payloads,
+// but let Axios set multipart boundaries automatically for FormData.
+api.interceptors.request.use((config) => {
+  if (config.data instanceof FormData) {
+    if (config.headers) {
+      delete config.headers["Content-Type"];
+    }
+  } else {
+    if (config.headers) {
+      config.headers["Content-Type"] = "application/json";
+    }
+  }
+  return config;
+});
+
+export default api;
